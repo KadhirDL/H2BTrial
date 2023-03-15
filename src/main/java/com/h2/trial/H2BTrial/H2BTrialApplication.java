8c94db9cfdc27eaa6d73f539afe00873b929070b
Kadhir D.L <kadhir.dasaian@oportun.com>
2023-03-15 16:38:12 +0530

    Initial commit

---
+++ src/main/java/com/h2/trial/H2BTrial/H2BTrialApplication.java
@@ -0,0 +1,13 @@
+package com.h2.trial.H2BTrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2BTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2BTrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/h2/trial/H2BTrial/H2BTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2.trial.H2BTrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2BTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
